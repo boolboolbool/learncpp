32d4769e091986326754b0047b62767634be5cde
boolx3 <boolboolbool@users.noreply.github.com>
2022-05-27 16:33:37 +1000

    docs(1.2): use C-style multi-line comments
    Switch multi-line comments to use /* */ C-style multi-line comments,
    plus stylize it with extra * for new lines inside
    
    	modified:   HelloWorld/main.cpp

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,7 @@
-// This first line of code is referred to as a preprocessor directive
-// This preprocessor directive indicates that we would like to use the contents
-// of the iostream library, which is the part of the C++ standard library that
-// allows us to read and write text from/to the console.
+/* This first line of code is referred to as a preprocessor directive
+ * This preprocessor directive indicates that we would like to use the contents  * of the iostream library, which is the part of the C++ standard library that
+ * allows us to read and write text from/to the console.
+ */
 #include <iostream>
 
 int main() // every program must have a main function or it will fail to link
@@ -10,9 +10,10 @@ int main() // every program must have a main function or it will fail to link
     // send letters or numbers to the console output.
     std::cout << "Hello world!" << std::endl;
 
-    // when an executable program finishe runing, the program sends a value back
-    // to the operating system in order to indicate whether it ran successfully
-    // or not.
-    // This statement returns 0 to the operating system, indicating success
+    /* when an executable program finishe runing, the program sends a value back
+     * to the operating system in order to indicate whether it ran successfully
+     * or not.
+     * This statement returns 0 to the operating system, indicating success
+     */
     return 0;
 }
